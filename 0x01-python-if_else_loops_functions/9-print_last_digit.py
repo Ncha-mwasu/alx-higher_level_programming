e964c2e105469bf184304dfd0335fafbca1ea534
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-20 11:56:38 -0700

    Python conditionals 09

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,9 +5,9 @@
 def print_last_digit(number):
     if number < 0:
         last_digit = (-1 * number) % 10
-  
+
     else:
         last_digit = number % 10
-  
+
     print(f"{last_digit}", end="")
     return last_digit
