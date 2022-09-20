a67fcbb217002d0d333e74eb755afcf638c86aa9
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-20 08:43:30 -0700

    Python Conditional 06

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for numbers_1 in range(0, 9):
+    for numbers_2 in range(1, 10):
+        if numbers_1 < numbers_2:
+            if numbers_1 == 8: 
+                print("{}{}".format(numbers_1, numbers_2), end="")
+            else:
+                print("{}{},".format(numbers_1, numbers_2), end=" ")
+print()
