7800e02f4f7a44ff5c9ae26b862edd57d5ad7589
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-20 07:25:10 -0700

    Python Conditional 02 & 03

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,5 @@
 letters = 97
 while letters < 123:
     if letters != 101 and letters != 113:
-        print("{}".format(chr(letters)), end = "")
+        print("{}".format(chr(letters)), end="")
     letters += 1
