12857060b663848a57e5cc2ffe9f326b098b45f2
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-20 16:07:54 -0700

    python conditionals

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for letters in range (122, 96, -1):
-  if letters % 2 == 0:
-    print("{}".format(chr(letters)), end="")
-  else:
-    print("{}".format(chr(letters - 32)), end="")
+    if letters % 2 == 0:
+        print("{}".format(chr(letters)), end="")
+    else:
+        print("{}".format(chr(letters - 32)), end="")
 
