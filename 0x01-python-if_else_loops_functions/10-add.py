3ea49888db30f72d8e31a9a3b1de945449e96751
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-20 12:04:38 -0700

    Python conditionals 10

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # add - adds two values
-# a: argument 1 
+# a: argument 1
 # b: argument 2
 # Return: the addition of both values
 
 def add(a, b):
-    eturn a + b
+    return a + b
