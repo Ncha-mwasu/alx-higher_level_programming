4d60956af912e103de9571afd86e435f155bc260
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-22 08:34:01 -0700

    Python modules 03

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    arg_len = len(argv)
+    Sum = 0
+
+    for args in range(1, arg_len):
+        Sum += int(argv[args])   
+    print("{}".format(Sum))
