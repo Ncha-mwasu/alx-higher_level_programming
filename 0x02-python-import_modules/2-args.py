acaa2360d508eafaa06df9917ea624eabf8fd2ed
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-22 07:59:18 -0700

    Python methods 02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-arg_count = len(argv) - 1
-if arg_count == 0:
-    print("{} arguments.".format(arg_count))
-else:
-    print("{} arguments:".format(arg_count))
-for count in range(arg_count):
-    print("{}: {}".format(count + 1, argv[count + 1]))
+    arg_count = len(argv) - 1
+    if arg_count == 0:
+        print("{} arguments.".format(arg_count))
+    else:
+        print("{} arguments:".format(arg_count))
+    for count in range(arg_count):
+        print("{}: {}".format(count + 1, argv[count + 1]))
