16331b1a20394f8f4e0c55f1d7a15a31ce7d5f7e
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-22 07:09:40 -0700

    Python modules 02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-
-arg_len = len(argv)
-for n in range(0, arg_len):
-    count = n
-print("{} {}:".format(count, argv[count]))
-for n in argv:
-    args = n
-print("{}: {}".format(count, args))
+arg_count = len(argv) - 1
+if arg_count == 0:
+    print("0 arguments.")
+else:
+    print("{} arguments:".format(arg_count))
+for count in range(arg_count):
+    print("{}: {}".format(count + 1, argv[count + 1]))
