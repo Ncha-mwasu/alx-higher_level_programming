b5595479645136a1b76cc8d1b641a6a64f2846fa
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-23 06:00:36 -0700

    Python Modules 04

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,10 @@
 if __name__ == "__main__":
     from hidden_4 import __init__, my_secret_santa, print_school, print_hidden
     builtins = dir()
+    # print(builtins)
     list_len = len(builtins)
     for items in range(0, list_len):
-        if items > 9:
-            print(builtins[items])
+        num = builtins[items]
+        if num[:2] != '__':
+            num = builtins[items]
+            print(num)
