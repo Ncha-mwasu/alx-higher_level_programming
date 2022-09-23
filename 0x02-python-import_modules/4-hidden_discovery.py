ffb1ac95a3588f23aef333e52ff41d7564767104
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-23 07:09:54 -0700

    Python modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
     import hidden_4
-    built = dir(hidden_4)
-    for name in built:
-        if name[:2] != "__":
-            print(name)
+built = dir(hidden_4)
+for name in built:
+    if name[:2] != "__":
+        print(name)
