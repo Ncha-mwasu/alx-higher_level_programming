1ea1f5750a6420eda8624bdd2809099e7bb9108f
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-25 15:41:57 -0700

    Python DS

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,9 @@
 def multiple_returns(sentence):
     string_len = len(sentence)
     if string_len == 0:
-        return None
+        str_tuple = (string_len, None)
+        return str_tuple
+
     else:
         first_char = sentence[0]
         str_tuple = (string_len, first_char)
