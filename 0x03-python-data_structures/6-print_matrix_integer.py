72eeeb9dd752aa1d578fc2859c172765faf689ee
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-25 08:55:33 -0700

    Python DS

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for lists in matrix:
         for rows in lists:
-            if rows == lists[(len(lists) -1)]:
+            if rows == lists[(len(lists) - 1)]:
                 print("{:d}".format(rows), end="")
             else:
                 print("{:d}".format(rows), end=" ")
