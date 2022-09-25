ce2c36fdac9e6123088485a57b29387755da884c
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-25 05:19:16 -0700

    Pyhton DS

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    list_len = len(my_list)
+    list_len = len(my_list) - 1
     for items in range(list_len, 0, -1):
         print("{}".format(items))
