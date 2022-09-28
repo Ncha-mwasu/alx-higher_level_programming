2ed7a1d8e496b29044a07772fbd910a13cc39d08
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-28 15:52:02 -0700

    Python DS

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,4 @@ def best_score(a_dictionary):
     if a_dictionary == None:
         return None
     else:
-        return max(a_dictionary)
+        return maxmax(a_dictionary, key=a_dictionary.get)
