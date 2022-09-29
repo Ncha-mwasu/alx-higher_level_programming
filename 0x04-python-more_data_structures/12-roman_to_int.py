46d70600de828774fb767972171c6d331d50ef04
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-28 21:58:49 -0700

    Python DS

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int_v = 0
+    for i in range(len(roman_string)):
+        if i > 0 and rom[roman_string[i]] > rom[roman_string[i - 1]]:
+            int_v += rom[roman_string[i]] - 2 * rom[roman_string[i - 1]]
+        else:
+            int_v += rom[roman_string[i]]
+    return int_v
