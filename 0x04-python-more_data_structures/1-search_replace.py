5de0aa636bba5aa36ac24a614de6e0258d30c1c8
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-29 00:48:52 -0700

    Python DS

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
     list_len = len(new_list)
-    for item in range(list_len - 1):
+    for item in range(list_len):
         if new_list[item] == search:
             new_list[item] = replace
     return new_list
