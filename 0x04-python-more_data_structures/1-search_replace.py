4a57eaef9e7f6b4c26b9c2a22618da135d3978e7
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-09-29 00:46:38 -0700

    Python DS

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    for numbers in new_list:
-        if numbers == search:
-            search = replace
-        return new_list
+    list_len = len(new_list)
+    for item in range(list_len - 1):
+        if new_list[item] == search:
+            new_list[item] = replace
+    return new_list
+
