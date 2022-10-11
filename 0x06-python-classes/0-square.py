6d3a3b9e2efafa30e141792f6f6428d11c7b6693
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-10-11 16:47:08 -0700

    Python Classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """Class square a blueprint (definition) for a square object
-    """
+    """Class square a blueprint (definition) for a square object"""
     pass
