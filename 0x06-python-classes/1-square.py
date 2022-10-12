99487264a0e5413c4776c6ab20523d9badb1ec02
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-10-11 17:01:12 -0700

    Python Classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Defining a class square"""
 
+
 class Square:
     """Class square a blueprint (definition) for a square object
     """
     def __init__(self, size):
-        """__init__ Class Initialization method that stores the size of the square
+        """Initialization method that stores the size of the square
         Args:
             size (int): size of the square
         """
