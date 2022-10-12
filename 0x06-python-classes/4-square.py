dae0f5cac7ca8fcc541f78d98d0a3a4a6ff024b3
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-10-11 17:38:33 -0700

    Python Classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,19 +3,13 @@
 
 
 class Square:
-    """Class square a blueprint (definition) for a square object
-    """
+    """Class square a blueprint (definition) for a square object"""
 
     def __init__(self, size=0):
         """method that stores the size of the square
         Args:
             param1 (int): the size of the square
         """
-        if not isinstance(size, int):
-            raise Exception("size must be an integer")
-        elif size < 0:
-            raise Exception("size must be >= 0")
-        else:
             self.__size = size
 
     def area(self):
