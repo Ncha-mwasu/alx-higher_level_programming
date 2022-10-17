25a8382f2e698366ee49c160ca2aba563f21039e
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-10-17 07:37:35 -0700

    Python Classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,15 +8,26 @@ class Rectangle:
     A class (blueprint) to create a rectangle.
     """
     def __init__(self, width=0, height=0):
+        """Initialize a new rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the rectangle.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """
+        Method to get the width of the triangle
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        Method to set the width of the rectangle
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -26,9 +37,15 @@ class Rectangle:
 
     @property
     def height(self):
+        """ 
+        Method to get the height of the rectangle
+        """
         return self.__height
 
     @height.setter
+    """
+    Method to set the height of the rectangle 
+    """
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
