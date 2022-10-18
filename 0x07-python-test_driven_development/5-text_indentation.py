e825e4619073599fcd0a97328bfb910f56a9142d
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-10-18 02:17:45 -0700

    Test Dev

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,30 @@
+#!/usr/bin/python3
+"""
+A module that prints out texts
+"""
 
+
+def text_indentation(text):
+    """Return text with 2 new lines after using '.?:'
+    Args:
+        param1: type str the text
+    Raise:
+        TypeError: If text is not of string type
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text) and text[i] == ' ':
+        i += 1
+
+    while i < len(text):
+        print(text[i], end='')
+        if text[i] == '\n' or text[i] in ".?:":
+            if text[i] in ".?:":
+                print('\n')
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
---
+++ 0x07-python-test_driven_development/test/6-max_integer_test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInt(unittest.TestCase):
+    """
+    TextMaxInt a class to test the max_integer modulue
+
+    testing against:
+    """
+
+    def test_max_integer(self):
+        """Positive integers"""
+        result = max_integer([1, 2, 3, 4])
+        counter = max([1, 2, 3, 4])
+        self.assertEqual(result, counter)
+
+    def test_max_integer1(self):
+        """Negative integers"""
+        result = max_integer([-1, -2, -3, -4])
+        counter = max([-1, -2, -3, -4])
+        self.assertEqual(result, counter)
+
+    def test_max_integer2(self):
+        """A mixture of positive and negative integers"""
+        result = max_integer([-1, 2, 3, -4])
+        counter = max([-1, 2, 3, -4])
+        self.assertEqual(result, counter)
+
+    def test_max_integer3(self):
+        """Positive floats"""
+        result = max_integer([1.4, 2.2, 3.5, 4.7])
+        counter = max([1.4, 2.2, 3.5, 4.7])
+        self.assertEqual(result, counter)
+
+    def test_max_integer4(self):
+        """Negative floats"""
+        result = max_integer([-1.4, -2.2, -3.5, -4.7])
+        counter = max([-1.4, -2.2, -3.5, -4.7])
+        self.assertEqual(result, counter)
+
+    def test_max_integer5(self):
+        """Positive and negative floats and integers"""
+        result = max_integer([-1.5, 2, 3, -4.6])
+        counter = max([-1.5, 2, 3, -4.6])
+        self.assertEqual(result, counter)
