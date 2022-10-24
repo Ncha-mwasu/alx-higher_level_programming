2d4ab80df3dd1e5f75a87b7a43be4dda96d549ec
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-10-24 03:04:16 -0700

    Python Inheritence

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+"""
+A module that returns a list
+"""
+
+
+def lookup(obj):
+    """method returning the properties of an instance of a class"""
+
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+"""
+A module that returns a list
+"""
+
+
+def lookup(obj):
+    print(dir(obj))
