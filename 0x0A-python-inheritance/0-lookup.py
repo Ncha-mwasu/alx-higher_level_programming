02a3892b1b8a5a00971a79f25994e12242e36eb1
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-10-24 05:27:48 -0700

    Python Inheritence

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def lookup(obj):
-    """
-    method returning all properties and methods
-    of the specified object, without the values
+    """Function returning all properties and methods
+        of the specified object, without the values
 
     Args:
-        Obj: a class instance
+        Obj: The class' instance
 
     Returns:
         The properties and methods of the specified object
