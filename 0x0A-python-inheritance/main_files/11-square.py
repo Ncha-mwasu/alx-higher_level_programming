e761ccec438caef07884fc5740d1eb8f2ced5a04
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-10-24 07:53:00 -0700

    Python Inheritence

---
+++ 0x0A-python-inheritance/main_files/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+"""
+    A class of name Square
+"""
+
+
+class Square(Rectangle):
+    """
+    A blueprint Rectangle class defined by its parent class 
+    Base geometry
+    """
+
+    def __init__(self, size):
+        """
+        Initialization of the class
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """
+        The square class return value
+        """
+        return "[Square] {0}/{0}".format(self.__size)
---
+++ 0x0A-python-inheritance/main_files/9-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+    A class of name Rectangle
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    A blueprint Rectangle class defined by its parent class 
+    Base geometry
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialization of the class
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Method to calculate the area of a rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {} / {}".format(self.__width, self.__height)
