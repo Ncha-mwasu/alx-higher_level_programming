f8694321d1874013c5d5af7d0434c37f55a8714e
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-10-25 06:34:42 -0700

    Python file handling

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -17,5 +17,4 @@ def append_write(filename="", text=""):
     """
 
     with open(filename, 'a', encoding='utf-8') as f:
-        f.write(text)
-        return f.tell()
+        return f.write(text)
