8f116fd8357fc088c93b2c087b796b4ebd3790c0
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-10-31 07:14:02 -0700

    Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,12 @@
 """
-
+A rectangle class inheriting
+from the base class
 """
 
 
+class Rectangle(base):
+    """
+    A class blueprints to initialize
+    the creation of a rectangle
+    """
+    
